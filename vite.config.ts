eadec669b4c79ac94f8d2a88cacaadd081a4ffe2
vjbhaskar <vj.bhaskar012@gmail.com>
2025-12-27 19:12:28 +0100

    add netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,35 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/test/setup.ts",
-    server: {
-      deps: {
-        inline: ["vuetify"],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode`
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue(), vuetify({ autoImport: true })],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/test/setup.ts",
+      server: {
+        deps: {
+          inline: ["vuetify"],
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    // Expose env variables to the app
+    define: {
+      "import.meta.env.VITE_API_BASE_URL": JSON.stringify(
+        env.VITE_API_BASE_URL
+      ),
+    },
+  };
 });
